259b15a968085777d6fb297fad837eb6271d4d78
sharmabpradeep <ps.sharma0579@gmail.com>
2024-01-29 14:14:23 +0530

    Prime gpt tailwind and basic structure of the app setup

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div className='bg-black h-10'>
+            <img
+            className='display-flex w-24 '
+            src="https://m.media-amazon.com/images/G/01/digital/video/web/Logo-min.png"
+            alt="Prime GPT Logo"
+            />
+        </div>
+        <h2>Header is here!</h2>
+    </div>
+  )
+}
+
+export default Header;
